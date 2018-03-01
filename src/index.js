1c14b53935b55c6455f67e1254750f881e10dfb2
Anastasiya Garbuz <36642340+StationToStation@users.noreply.github.com>
2018-03-01 15:58:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr=str.split(''), i, j;
+  let difficulty = bracketsConfig.length;
+  let lengthBeforeChecking=0 /*, lengthAfterChecking=0*/;
+  while (arr.length!=lengthBeforeChecking)
+  {
+    lengthBeforeChecking=arr.length;
+    for(i=0; i<lengthBeforeChecking; i++)
+    for(j=0; j<difficulty; j++)
+    {
+      if((arr[i]==bracketsConfig[j][0]) && (arr[i+1]==bracketsConfig[j][1])){
+      arr.splice(i,2); break;``
+      }
+    }
+  }
+ // arr.slice(0,4);
+  if (arr.length==0) return true;
+  else return false;
 }
